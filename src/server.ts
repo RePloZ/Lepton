c11f35a63c13bfb7b76eecd577e93c11f0faf022
Herve Kay <herve.kay@ynov.com>
2020-12-21 18:57:09 +0100

    :feature: add helmet for security

--- src/server.ts
+++ src/server.ts
@@ -5,15 +5,23 @@ import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
 import schema from './schema';
+import helmet from 'helmet';
+
 const app = express();
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
 });
+
+app.use(helmet());
 app.use('*', cors());
 app.use(compression());
+
 server.applyMiddleware({ app, path: '/graphql' });
+
 const httpServer = createServer(app);
+
 httpServer.listen(
   { port: 3000 },
-  (): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
+  (): void => console.log(`\n🚀 GraphQL is now running on http://localhost:3000/graphql`)
+);
